e04423378a9fa478a65ea14a21aa2198b5d2dfc4
joanna <j.baranowska@hotmail.com>
2023-07-27 11:52:08 +0200

    host home page

---
+++ src/main/java/com/example/live_agenda_front/LiveAgendaFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.live_agenda_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveAgendaFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveAgendaFrontApplication.class, args);
+    }
+
+}
